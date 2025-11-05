b41fa03c7cbe56ef7ff93a99d0b5d78222bd772b
Apple <apple@Apples-MacBook-Pro.local>
2025-11-05 20:27:53 +0800

    ajil

---
+++ ajil/js/ajil.js
@@ -0,0 +1,21 @@
+const links = document.querySelectorAll(".openLink");
+const modal = document.getElementById("myModal");
+const closeModal = document.getElementById("closeModal");
+
+links.forEach(link => {
+    link.addEventListener("click", function(event) {
+        event.preventDefault(); // stop the link from navigating
+        modal.style.display = "block"; // show modal
+        console.log("hello")
+    });
+});
+
+closeModal.addEventListener("click", function() {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+    if(event.target == modal) {
+        modal.style.display = "none";
+    }
+});
---
+++ ajil/js/home_page.js
@@ -0,0 +1,4 @@
+document.getElementById("hun").addEventListener("click", function() {
+    window.location.href = "ajil.html";});
+document.getElementById("ajil").addEventListener("click", function() {
+    window.location.href = "hun.html";});
