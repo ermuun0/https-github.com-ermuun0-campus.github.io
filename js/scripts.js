d868f6ad87c6b52c0261fe4fa8d50558d041036a
Apple <apple@Apples-MacBook-Pro.local>
2025-11-09 23:00:50 +0800

    add

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,48 @@
-document.getElementById("hun").addEventListener("click", function() {
-    window.location.href = "ajil.html";});
-document.getElementById("ajil").addEventListener("click", function() {
-    window.location.href = "hun.html";});
+// Get elements
+const openPopup = document.getElementById('profile');
+const popup = document.getElementById('myModal');
+const closePopup = document.getElementById('closeModal');
+
+// Open popup when div is clicked
+openPopup.addEventListener('click', () => {
+  popup.style.display = 'flex';
+});
+
+// Close popup when X is clicked
+closePopup.addEventListener('click', () => {
+  popup.style.display = 'none';
+});
+
+// Close popup when clicking outside the content
+window.addEventListener('click', (e) => {
+  if (e.target === popup) {
+    popup.style.display = 'none';
+  }
+});
+
+// Get elements
+const openU = document.getElementById('openPopup');
+const popU = document.getElementById('popup');
+const closeU = document.getElementById('closePopup');
+
+// Open popup when div is clicked
+openU.addEventListener('click', () => {
+  popU.style.display = 'flex';
+});
+
+// Close popup when X is clicked
+closeU.addEventListener('click', () => {
+  popU.style.display = 'none';
+});
+
+// Close popup when clicking outside the content
+window.addEventListener('click', (e) => {
+  if (e.target === popU) {
+    popU.style.display = 'none';
+  }
+});
+
+
 
 
 
